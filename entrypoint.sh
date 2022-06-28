dda0dbdcb6e4c793a2af90f187ed98ea1d28264d
Chiupam <65508083+chiupam@users.noreply.github.com>
2022-06-29 02:04:18 +0800

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,11 @@ start() {
     git clone https://github.com/chiupam/jd_sign.git /app >/dev/null 2>&1
     git -C /app fetch --all >/dev/null 2>&1
     git -C /app checkout main >/dev/null 2>&1
-    rm -rf /app/.idea >/dev/null 2>&1
-    rm -rf /app/.github >/dev/null 2>&1
-    rm -rf $(ls| egrep -v '(app.py|sign.py|ecosystem.config.js)') >/dev/null 2>&1
+    rm -rf /app/.idea /app/.github >/dev/null 2>&1
+    rm -rf $(ls | egrep -v "py|js") >/dev/null 2>&1
   fi
   echo "初始化完成, 启动..."
-  pm2-runtime start /app/ecosystem.config.js
+  pm2-runtime start /app/ecosystem.config.js >/dev/null 2>&1
 }
 
 start
