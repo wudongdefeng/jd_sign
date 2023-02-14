79fdc19df0f36c0d3a01fdf562cc21b75709097d
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2023-02-14 18:37:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 start() {
   if [ ! -d "/app/.git" ]; then
     echo "未检测到 sign 仓库, 正在克隆中, 请耐心等待..."
-    git clone https://git.dc-wind.ml/https://github.com/wudongdefeng/jd_sign.git /app >/dev/null 2>&1
+    git clone https://github.com/xzb1/jd_sign.git /app >/dev/null 2>&1
     git -C /app fetch --all >/dev/null 2>&1
     git -C /app checkout main >/dev/null 2>&1
     rm -rf /app/.idea /app/.github >/dev/null 2>&1
