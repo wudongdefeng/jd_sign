375a9a1c4a9eb6d2f63d08192a38081cdae0713b
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2023-02-16 20:26:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 start() {
   if [ ! -d "/app/.git" ]; then
     echo "未检测到 sign 仓库, 正在克隆中, 请耐心等待..."
-    git clone https://github.com/wudongdefeng/jd_sign.git /app >/dev/null 2>&1
+    git clone http://45.89.234.100:8080/https://github.com/wudongdefeng/jd_sign.git /app >/dev/null 2>&1
     git -C /app fetch --all >/dev/null 2>&1
     git -C /app checkout main >/dev/null 2>&1
     rm -rf /app/.idea /app/.github >/dev/null 2>&1
